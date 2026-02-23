bc79b8a20c822f8163770d59f69450861550ac62
ldphuong20122003 <ldphuong20122003@gmail.com>
2026-02-23 21:23:54 +0700

    rename Controllers and add getProfile and updatePassword

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const authRouter = require("./routers/authRouter");
+const userRouter = require("./routers/userRouter");
 const connectDb = require("./configs/connectDb");
 const errorMiddleware = require("./middlewares/errorMiddleware");
 
@@ -11,6 +12,7 @@ app.use(cors());
 const PORT = 3000;
 
 app.use("/auth", authRouter);
+app.use("/user", userRouter);
 
 connectDb();
 
--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,5 +1,5 @@
 const Router = require("express");
-const authController = require("../controlers/authController");
+const authController = require("../controllers/authController");
 
 const authRouter = Router();
 
